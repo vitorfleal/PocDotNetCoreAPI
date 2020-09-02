9a4fe4b443ff9db046b24c0c855e6e12d65669fb
vitor.leal <vitor.leal@pinpag.com.br>
2020-09-02 13:45:07 -0300

    Add project files.

---
+++ src/AE.HealthSystem.Domain/Entities/Medico.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AE.HealthSystem.Domain.Entities
+{
+    public class Medico : Pessoa
+    {
+        public Medico(string especialidade)
+        {
+            Especialidade = especialidade;
+        }
+
+        public Medico() {}
+
+        public string Especialidade{ get; private set; }
+
+        public virtual ICollection<Consulta> Consultas { get; set; }
+
+    }
+}
